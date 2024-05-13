4ee029d1d4adfb35c3d6426e56ef68637e227cc0
Mehmet Balkan <mbalkan@meyermeyer.com>
2024-05-13 12:52:27 +0200

    erster commit

--- chess.py
+++ chess.py
@@ -9,7 +9,7 @@ WIDTH, HEIGHT = 800, 800
 BOARD_SIZE = 8
 SQUARE_SIZE = WIDTH // BOARD_SIZE
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+BLACK = (0, 0, 0) 
 GRAY = (128, 128, 128)
 
 # Create the screen
